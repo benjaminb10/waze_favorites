a549478e7fbcd48f90be664ebc9d4163b6490bf5
Benjamin Benoudis <benjamin.benoudis@gmail.com>
2015-09-13 04:41:07 +0200

    Add Waze api

--- app/app/controllers/pages_controller.rb
+++ app/app/controllers/pages_controller.rb
@@ -1,2 +1,12 @@
 class PagesController < ApplicationController
+	include HTTParty
+	def index
+
+		headers = {
+    	"X-Mashape-Key" => "Yn5LVRJsHUmsh6WCNPlt1XjJRTp5p1XEpTTjsn4kWhIpnXgSaB",
+    	"Accept" => "application/json"
+  	}
+
+		@response = HTTParty.get('https://bestapi-waze-free-v1.p.mashape.com/routes?end=85+rue+chaptal%2C+levallois-perret&start=32+rue+la+boetie%2C+paris', :headers => headers)
+	end
 end
