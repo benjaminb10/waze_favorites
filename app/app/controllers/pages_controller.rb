32a22cf4e8cf725600fafbac87be71439e10b76a
Benjamin Benoudis <benjamin.benoudis@gmail.com>
2015-09-15 17:24:25 +0200

    Improve api

--- app/app/controllers/pages_controller.rb
+++ app/app/controllers/pages_controller.rb
@@ -2,11 +2,50 @@ class PagesController < ApplicationController
 	include HTTParty
 	def index
 
-		headers = {
-    	"X-Mashape-Key" => "Yn5LVRJsHUmsh6WCNPlt1XjJRTp5p1XEpTTjsn4kWhIpnXgSaB",
-    	"Accept" => "application/json"
-  	}
+		@departure = params[:departure].gsub!(' ', '%20') if params[:departure]
+		@arrivate = params[:arrivate].gsub!(' ', '%20') if params[:arrivate]
 
-		@response = HTTParty.get('https://bestapi-waze-free-v1.p.mashape.com/routes?end=85+rue+chaptal%2C+levallois-perret&start=32+rue+la+boetie%2C+paris', :headers => headers)
+		if @departure and @arrivate
+			headers = {
+	    	"X-Mashape-Key" => "Yn5LVRJsHUmsh6WCNPlt1XjJRTp5p1XEpTTjsn4kWhIpnXgSaB",
+	    	"Accept" => "application/json"
+	  	}
+
+	  	base_uri = "https://bestapi-waze-free-v1.p.mashape.com/routes?end=#{@arrivate}&start=#{@departure}"
+
+			@response = HTTParty.get(base_uri, :headers => headers).parsed_response["routes"][0]["routeDurationInMinutes"]
+		else
+			@response = 'caca'
+		end
+
+		# json = '{
+		#   "routes": [
+		#     {
+		#       "routeName": "A6a - E5; PÃ©riphÃ©rique IntÃ©rieur Ouest",
+		#       "routeDurationInMinutes": 24,
+		#       "routeLengthKM": 25.579,
+		#       "routeLengthMiles": 15.894048809000001,
+		#       "toll": false
+		#     },
+		#     {
+		#       "routeName": "PÃ©riphÃ©rique IntÃ©rieur Sud; PÃ©riphÃ©rique IntÃ©rieur Ouest",
+		#       "routeDurationInMinutes": 32,
+		#       "routeLengthKM": 23.306,
+		#       "routeLengthMiles": 14.481672526,
+		#       "toll": false
+		#     }
+		#   ],
+		#   "startPoint": "Thiais",
+		#   "endPoint": "85 Rue Chaptal, 92300 Levallois-Perret",
+		#   "startLatitude": "48.760541406276246",
+		#   "startLongitude": "2.387377464996807",
+		#   "endLatitude": "48.76042906",
+		#   "endLongitude": "2.38678763"
+		# }'
+
+		# @response = JSON.parse(json)
+
+
+
 	end
 end
--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
+  match '/', to: 'pages#index', via: :post, as: 'pages-index'
 end
